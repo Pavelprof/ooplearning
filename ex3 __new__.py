96d76855c13fd2571ac004f335704fb7d1a6b5bd
Pavel <34599280+Pavelprof@users.noreply.github.com>
2023-05-23 10:12:10 +0300

    __new__

---
+++ ex3 __new__.py
@@ -0,0 +1,8 @@
+class Point:
+    def __new__(cls, *args, **kwargs):
+        print('call __new__ for' + str(cls))
+
+    def __init__(self, x=0, y=0):
+        print('call __init__ for' + str(self))
+        self.x=x
+        self.y=y
