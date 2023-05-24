eb74858201a946abb30d79048d442cc190a1576a
Pavel <34599280+Pavelprof@users.noreply.github.com>
2023-05-24 09:53:38 +0300

    __new__ 2

--- ex3 __new__.py
+++ ex3 __new__.py
@@ -1,8 +1,13 @@
 class Point:
     def __new__(cls, *args, **kwargs):
         print('call __new__ for' + str(cls))
+        return super().__new__(cls)
 
-    def __init__(self, x=0, y=0):
+    def __init__(self, x=5, y=6):
         print('call __init__ for' + str(self))
         self.x=x
         self.y=y
+
+pt = Point(3,4)
+print(pt)
+print(pt.x)
