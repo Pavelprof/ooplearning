432924235549be8b9cc2b855320721425a40431e
Pavel <34599280+Pavelprof@users.noreply.github.com>
2023-05-29 10:15:56 +0300

    ex 5

--- ex5.py
+++ ex5.py
@@ -1,7 +1,20 @@
 class Vector:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    MIN_COORD = 0
+    MAX_COORD = 100
+
+    @classmethod
+    def validate(cls, arg):
+        return cls.MIN_COORD <= arg <= cls.MAX_COORD
+
+    def __init__(self, a, b):
+        self.x = self.y = 0
+        if self.validate(a) and self.validate(b):
+            self.x = a
+            self.y = b
 
     def get_coords(self):
         return self.x, self.y
+
+v = Vector(11,7)
+res = Vector.get_coords(v)
+print(res)
