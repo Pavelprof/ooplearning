9de06edc5c491eebb0aa107252879f231c43cbcc
Pavel <34599280+Pavelprof@users.noreply.github.com>
2023-06-05 18:35:11 +0300

    encapsulation 1 public, private, protected attributes

--- ex6_encapsulation.py
+++ ex6_encapsulation.py
@@ -1,11 +1,17 @@
+from accessify import private, protected
 class Point:
     def __init__(self, x=0, y=0):
-        self.__x = x
-        self.__y = y
-        print(self.__x, self.__y)
+        self.__x = self.__y = 0
+        if self.__check_value(x) and self.__check_value(y):
+            self.__x = x
+            self.__y = y
 
+    @private
+    @classmethod
+    def __check_value(cls, x):
+        return type(x) in (int, float)
     def set_coords(self, x, y):
-        if type(x) in (int, float) and type(y) in (int, float):
+        if self.__check_value(x) and self.__check_value(y):
             self.__x = x
             self.__y = y
         else:
@@ -14,8 +20,8 @@ class Point:
     def get_coords(self):
         return self.__x, self.__y
 
-pt = Point(1,2)
-pt.set_coords(10,4)
+pt = Point('4',2)
 print(pt.get_coords())
-
-print(type(float))
+print(pt._Point__x)
+print(dir(pt))
+print(pt._Point__check_value(1001))
