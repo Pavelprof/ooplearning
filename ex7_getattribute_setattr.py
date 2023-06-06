c0b764c799da7d3a9ac20c62dba78f5808d532ea
Pavel <34599280+Pavelprof@users.noreply.github.com>
2023-06-06 19:18:07 +0300

    __getattribute__

--- ex7_getattribute_setattr.py
+++ ex7_getattribute_setattr.py
@@ -17,6 +17,17 @@ class Point:
         else:
             return object.__getattribute__(self, item)
 
+    def __setattr__(self, key, value):
+        if key == 'z':
+            raise AttributeError("недопустимое значение")
+        else:
+            print('__setattr__')
+#            self.__dict__[key] = value
+            object.__setattr__(self, key, value)
+
+    def __getattr__(self, item):
+        return False
+
 pt1 = Point(1,2)
 pt2 = Point(10,20)
-a = pt1.y
+print(pt1.MAX_COORD1)
