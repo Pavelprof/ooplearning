70c6bff61918610876f0b11122a8181977ff8e2b
Pavel <34599280+Pavelprof@users.noreply.github.com>
2023-06-07 14:30:22 +0300

    ex 10 new

--- ex10_class_example.py
+++ ex10_class_example.py
@@ -4,6 +4,10 @@ class Person:
     S_RUS_UPPER = S_RUS.upper()
 
     def __init__(self, fio, old, ps, weight):
+        self.verify_fio(fio)
+        self.verify_old(old)
+        self.verify_weight(weight)
+        self.verify_ps(ps)
         self.__fio = fio.split()
         self.__old = old
         self.__passport = ps
@@ -21,5 +25,28 @@ class Person:
         letters = ascii_letters + cls.S_RUS + cls.S_RUS_UPPER
         for s in f:
             if len(s) < 1:
-                raise TypeError('Type at least one simbol')
-            if
+                raise TypeError('Please, type at least one character')
+            if len(s.strip(letters)) != 0:
+                raise TypeError('Please, use only allowed characters')
+
+    @classmethod
+    def verify_old(cls, old):
+        if type(old) != int or old < 14 or old >120:
+            raise TypeError('Age should be integer between 14 and 120')
+
+    @classmethod
+    def verify_weight(cls, w):
+        if type(w) not in (int, float) or w < 20:
+            raise TypeError('Weight should be integer or float from 20')
+
+    @classmethod
+    def verify_ps(cls, ps):
+        if type(ps) != str:
+            raise TypeError('Passport must be string')
+
+        s = ps.split()
+        if len(s) != 2 or len(s[0]) != 4 or len(s[1]) !=6:
+            raise TypeError('Wrong passport forma')
+
+
+p = Person('ВАсилукп Павеле Ленавмвваи', 33, '3234 234543', 80)
