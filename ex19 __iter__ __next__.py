dfc3db86f7f555e81011672ff7fb04681836c221
Pavel <34599280+Pavelprof@users.noreply.github.com>
2023-06-14 15:55:48 +0300

    ex19 and ex20

--- ex19 __iter__ __next__.py
+++ ex19 __iter__ __next__.py
@@ -1,13 +1,12 @@
-print(list(range(0, 4, 1)))
-a9 = iter(range(5))
-next(a9)
-
 class FRange:
     def __init__(self, start=0.0, stop=0.0, step=1.0):
         self.start = start
         self.stop = stop
         self.step = step
+
+    def __iter__(self):
         self.value = self.start - self.step
+        return self
 
     def __next__(self):
         if self.value + self.step < self.stop:
@@ -16,9 +15,27 @@ class FRange:
         else:
             raise StopIteration
 
-fr = FRange(0, 2, 0.5)
-print(fr.__next__())
-print(fr.__next__())
-print(next(fr))
-print(next(fr))
-print(next(fr))
+
+class FRange2D:
+    def __init__(self, start=0.0, stop=0.0, step=1.0, rows=5):
+        self.rows = rows
+        self.fr = FRange(start, stop, step)
+
+    def __iter__(self):
+        self.value = 0
+        return self
+
+    def __next__(self):
+        if self.value < self.rows:
+            self.value += 1
+            return iter(self.fr)
+        else:
+            raise StopIteration
+
+
+fr = FRange2D(0, 3, 0.5, 4)
+for row in fr:
+    for x in row:
+        print(x, end=" ")
+    print()
+
