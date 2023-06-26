9f768299e2b9963b6aea7f7abffb004871e8cb5e
Pavel <34599280+Pavelprof@users.noreply.github.com>
2023-06-26 06:56:39 +0300

    ex30_done_ex31_in_progress

--- ex31_raise.py
+++ ex31_raise.py
@@ -1,18 +1,8 @@
-def func2():
-    return 1/0
-
-def func1():
-    return func2()
-print('egrgt 335tt3rherh 3h 35h3 h3')
 print('egrgt 3rherh 3h5t54 35h3 h3')
 print('egrgt 3rherergrh 3h 35h3 h3')
 print('egrgt 3rergherh 3h 35h3 h3')
 print('egrgtege 3rherh 3h 35h3 h3')
-try:
-    func1()
-except:
-    print('func1 error')
+1/0
 print('egrgt 3rherhsvfe 3h 35h3 h3')
 print('egrgt 3rherh 3h 35qegeh3 h3')
 print('egrgt 3rherh 3h 35h3 h3')
-print('egrgt 3rherh wegre3h 35h3 h3')
