16f7c966823875de937679f36a0b5b620d78ab4c
Pavel <34599280+Pavelprof@users.noreply.github.com>
2023-07-01 23:57:14 +0300

    ex34 is 'Done' ex35 is 'in progress'

--- ex34_MetaClasses.py
+++ ex34_MetaClasses.py
@@ -1,3 +1,17 @@
 class Point:
     MAX_COORD = 100
     MIN_COORD = 0
+
+class B1: pass
+class B2: pass
+
+def method1(self):
+    print(self.__dict__)
+
+A = type('Point', (B1, B2), {"MAX_COORD" : 100, "MIN_COORD" : 0, "method1" : method1})
+
+pt = A()
+
+pt.method1()
+
+A.__mro__
---
+++ ex35_MetaClasses2.py
@@ -0,0 +1,3 @@
+class Point:
+    MAX_COORD = 100
+    MIN_COORD = 0
