938c45904c4ed4003878a0baa484fbe3acd4e31e
Pavel <34599280+Pavelprof@users.noreply.github.com>
2023-07-02 22:38:43 +0300

    ex36 is 'Done'

---
+++ ex36_Metaclasses_in_API_ORM_Django.py
@@ -0,0 +1,16 @@
+class Meta(type):
+    def create_local_attrs(self, *args, **kwargs):
+        for key, value in self.class_attrs.items():
+            self.__dict__[key] = value
+    def __init__(cls, name, base, attrs):
+        cls.class_attrs = attrs
+        cls.__init__ = Meta.create_local_attrs
+
+
+class Women(metaclass=Meta):
+    title = 'title'
+    content = 'content'
+    photo = 'path to photo'
+
+w = Women()
+print(w.__dict__)
