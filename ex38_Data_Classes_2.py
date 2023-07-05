f9afb73c330decb89096a759add01f276e6d5aad
Pavel <34599280+Pavelprof@users.noreply.github.com>
2023-07-05 17:39:46 +0300

    ex38 is 'In progress'

--- ex38_Data_Classes_2.py
+++ ex38_Data_Classes_2.py
@@ -1,7 +1,24 @@
 from dataclasses import dataclass, field
+
+class Vector3D:
+    def __init__(self, x: int, y: int, z: int, calc_len: bool = True):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.length = (x * x + y * y + z * z) ** 0.5 if calc_len else 0
+
 @dataclass
-class ThingData:
-    name: str
-    weight: int
-    price: float = 0
-    dims: list = field(default_factory=list)
+class V3D:
+    x: int = field(repr=False)
+    y: int
+    z: int = field(compare=False)
+    length: float = field(init=False, compare=False)
+
+    def __post_init__(self):
+        self.length = (self.x * self.x + self.y * self.y + self.z * self.z) ** 0.5
+
+v = V3D(1,2,3)
+
+print(v)
+print(v.length)
+print(v.__dict__)
