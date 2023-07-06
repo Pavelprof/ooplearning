b649f9a08f9e4d232f71433b5feb3f98e0a72a8f
Pavel <34599280+Pavelprof@users.noreply.github.com>
2023-07-06 18:04:08 +0300

    ex39 is "In progress" 1

--- ex39_Data_Classes_inheritance.py
+++ ex39_Data_Classes_inheritance.py
@@ -1,6 +1,12 @@
 from dataclasses import dataclass, field, InitVar
 from typing import Any
 
+class GoodsMethodsFactory:
+    @staticmethod
+    def get_init_measure():
+        return [0,0,0]
+
+
 @dataclass
 class Goods:
     current_uid = 0
@@ -9,15 +15,41 @@ class Goods:
     price: Any = None
     weight: Any = None
 
+    def __post_init__(self):
+        print("Goods: __post_init__")
+        Goods.current_uid += 1
+        self.uid = Goods.current_uid
+
 @dataclass
 class Book(Goods):
     title: str = ''
     author: str = ''
     price: float = 0
     weight: int | float = 0
+    measure: list = field(default_factory=GoodsMethodsFactory.get_init_measure)
 
-b = Book(123, 200, 1, 'bookk', 'Jason')
+    @staticmethod
+    def get_init_measure():
+        return [0,0,0]
+
+    def __post_init__(self):
+        super().__post_init__()
+        print("Book: __post_init__")
+
+b = Book(200, 1, 'bookk', 'Jason')
 print(b)
 
 c = Book(1)
 print(c)
+
+
+class Car:
+    def __init__(self, model, max_speed, price):
+        self.model = model
+        self.max_speed = max_speed
+        self.price = price
+
+    def get_max_speed(self):
+        return self.max_speed
+
+
