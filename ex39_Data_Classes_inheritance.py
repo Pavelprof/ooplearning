3adc091fa62a47a16cc1ebdd0b3ad9c2f20a02a0
Pavel <34599280+Pavelprof@users.noreply.github.com>
2023-07-06 10:16:39 +0300

    ex39 is "In progress"

--- ex39_Data_Classes_inheritance.py
+++ ex39_Data_Classes_inheritance.py
@@ -3,6 +3,21 @@ from typing import Any
 
 @dataclass
 class Goods:
-    uid: Any
+    current_uid = 0
+
+    uid: int = field(init = False)
     price: Any = None
     weight: Any = None
+
+@dataclass
+class Book(Goods):
+    title: str = ''
+    author: str = ''
+    price: float = 0
+    weight: int | float = 0
+
+b = Book(123, 200, 1, 'bookk', 'Jason')
+print(b)
+
+c = Book(1)
+print(c)
