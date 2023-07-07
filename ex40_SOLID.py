6f223e488168ae1832228cb1c735778617e0281f
Pavel <34599280+Pavelprof@users.noreply.github.com>
2023-07-07 16:43:44 +0300

    ex40 is "Done"

---
+++ ex40_SOLID.py
@@ -0,0 +1,6 @@
+# Single Responsibility Principle (Принцып единой ответственности)
+# Open-Closed Principle (принцип открытости закрытости)
+# Liskov substitution Principle (принцып подставновки Барбары Лисков)
+# Interface Segregation Principle (принцып разделения интерфейса)
+# Dependency Inversion Principle (принцып инверсии зависимостей)
+
